f749afbbc3fe244058acf5a52a5fdbb737e5f290
unknown <krishanb281988@gmail.com>
2024-09-15 12:11:05 +0530

    tt

--- variables.tf
+++ variables.tf
@@ -1,7 +1,21 @@
-variable "VPC_CIDR" {}
+/*variable "VPC_CIDR" {}
 variable "ENV" {}
 
 variable "PUBLIC_SUBNET_CIDR" {}
 variable "PRIVATE_SUBNET_CIDR" {}
 variable "AZ" {}
+*/
+
+variable "ENV" {
+  type = string
+}
+variable "PUBLIC_SUBNET_CIDR" {
+  type = string
+}
+variable "PRIVATE_SUBNET_CIDR" {
+  type = string
+}
+variable "AZ" {
+  type = string
+}
 
